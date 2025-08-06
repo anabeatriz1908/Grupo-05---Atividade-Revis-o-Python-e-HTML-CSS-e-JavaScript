ffaf20fbe1873ee03161dba6383f29a0a164ba18
Ana Beatriz Silva Santos <ana1908beatrizsantos@gmail.com>
2025-08-06 08:58:48 -0300

    JS funcionando

--- main.js
+++ main.js
@@ -18,9 +18,30 @@ async function calculadoraImc() {
     console.log("peso: " + peso);
 
     try { 
-        let imc = 
+        let imc = peso / (altura * altura)
+        let resultado = ""
+
+
+    if(imc < 18.5){
+        resultado = "Abaixo do Peso"
+    } else if(imc >= 18.5 && imc < 24.9){
+        resultado = "Normal"
+    } else if(imc >= 25 && imc <= 29.9){
+        resultado = "Sobrepeso"
+    } else if(imc >=30 && imc <=34.9){
+        resultado = "Obesidade grau I"
+    } else if(imc >=35 && imc <=39.9){
+        resultado = "Obesidade grau II"
+    } else{
+        resultado = "Obesidade grau III"
+    }
+
+
+
+    alert(`Seu IMC é: ${resultado}`)
 
     }catch{
+        alert('Erro ao calcular o IMC')
 
     }
 }
